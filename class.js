f052ecf246d6304338a68fce47366a0b45a4208b
Dariusz <darek10121998db@gmail.com>
2021-01-24 22:10:30 +0100

    index.html class.js models20

---
+++ class.js
@@ -0,0 +1,15 @@
+
+var model;
+async function initialize() {
+    model = await tf.loadLayersModel('/models/model.json');
+    model.summary();
+    console.log(model)
+}
+window.predict = async(image) => {
+    // action for the submit button
+    console.log(tf.browser.fromPixels(image))
+    let tensorImg = tf.browser.fromPixels(image).resizeNearestNeighbor([180, 180]).toFloat().expandDims();
+    prediction = await model.predict(tensorImg).data();
+    return prediction;
+}
+initialize();
